f6d17dc47b30d9f4d49595ef3d5fb2f63c85bffa
kth <dms304@outlook.kr>
2026-06-01 01:58:53 +0900

    msk vpc cidr을 eks 클러스터 vpc cidr로 변경

--- src/main/java/org/tama/tamaapi/controller/HealthCheckApiController.java
+++ src/main/java/org/tama/tamaapi/controller/HealthCheckApiController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HealthCheckApiController {
 
+
     @GetMapping("/api/item/health-check/ok")
     public String healthOk() {
         log.info("ok");
